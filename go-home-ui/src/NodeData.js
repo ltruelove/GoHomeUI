6924a44654135960d5dda461f5c7564d5aaa648f
ltruelove <levitruelove@gmail.com>
2022-10-18 12:04:12 -0400

    Add the ability to toggle a node switch on the node's detail page

--- go-home-ui/src/NodeData.js
+++ go-home-ui/src/NodeData.js
@@ -8,17 +8,54 @@ class NodeData extends Component{
         this.state = {
             recordId : props.id,
             record: {},
+            data: null,
             allSensors: [],
-            allSwitches: []
+            allSwitches: [],
+            pin: ""
         };
 
         this.deleteNodeClick = this.deleteNode.bind(this);
+        this.getNodeData = this.getNodeData.bind(this);
+        this.handlePIN = this.handlePIN.bind(this);
+        this.toggleButton = this.toggleButton.bind(this);
+    }
+
+    handlePIN = event => {
+        this.setState({ pin: event.target.value });
+    };
+
+    getNodeData(nodeId){
+        axios.get('http://' + this.state.record.controlPointIp + '/nodeData?nodeId=' + nodeId)
+        .then(res=>{
+            this.setState({data: res.data});
+        })
+        .catch(err=>console.log(err))
+    }
+
+    toggleButton(){
+        const requestBody = JSON.stringify({"pinCode" : this.state.pin});
+        const url = process.env.REACT_APP_API_URL + '/node/switch/toggle/' + this.state.recordId;
+        let id = this.state.recordId;
+
+        axios.post(url, requestBody)
+        .then(res=>{
+            console.log(res);
+            this.setState({data: res.data}, () => {
+                setTimeout(() => {
+                    this.getNodeData(id);
+                }, 100);
+            });
+        })
+        .catch(err=>console.log(err))
     }
 
     componentDidMount(){
         axios.get(process.env.REACT_APP_API_URL + '/node/' + this.state.recordId)
         .then(res=>{
-            this.setState({record: res.data})
+            console.log(res);
+            this.setState({record: res.data}, () => {
+                this.getNodeData(res.data.Id);
+            })
         })
         .catch(err=>console.log(err))
     }
@@ -38,12 +75,31 @@ class NodeData extends Component{
     }
 
     render(){
+        let data = <></>;
+
+        if(this.state.data){
+            data = <>
+                <p>Humidity: {this.state.data.Humidity}</p>
+                <p>Temperature F: {this.state.data.TemperatureF}</p>
+                <p>Temperature C: {this.state.data.TemperatureC}</p>
+                <p>Moisture: {this.state.data.Moisture}</p>
+                <p>Resistor Value: {this.state.data.ResistorValue}</p>
+                <p>Toggle Closed: {this.state.data.IsClosed ? "Yes":"No"}</p>
+                </>
+        }
+
         return (
             <>
             <p>Name: {this.state.record.Name}</p>
             <p>IP Address: {this.state.record.IpAddress}</p>
             <p>MAC Address: {this.state.record.Mac}</p>
+            {data}
             <button id={this.props.id} onClick={this.deleteNodeClick}>Delete</button>
+            <button onClick={() => this.getNodeData(this.state.recordId)}>Details</button>
+            <br />
+            <p>PIN: <input onChange={this.handlePIN} type="password" id="requestPIN" /></p>
+            
+            <button onClick={this.toggleButton}>Toggle Button</button>
             </>
         )
     }
